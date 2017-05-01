20460a58b231c56145f31d6821f4858e92b1d0ac
Omar Mason <mr.omarmason@gmail.com>
2017-05-01 16:54:09 +0000

    made it easier to write a review form a restrooms page.

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -12,6 +12,17 @@ class ReviewController < ApplicationController
      end
    end
 
+   get '/review/new/:id' do
+     if logged_in?
+       @restroom = Restroom.find(params[:id])
+       @user = current_user
+       @logged = logged_in?
+       erb :'/review/write_review'
+     else
+       redirect to '/login'
+     end
+  end
+
    get '/review/:id' do
      if logged_in?
        @user = current_user
@@ -24,18 +35,25 @@ class ReviewController < ApplicationController
    end
 
    post '/new/review' do
-     if !params[:review][:body].blank?
+     if !params[:review][:stars].blank?
        @user = current_user
        review =Review.create(params[:review])
 
        if !params[:restroom][:restaurant_name].blank? && !params[:restroom][:location].blank?
          review.restroom = Restroom.create(params[:restroom])
        end
+
        review.save
        redirect to "/review/#{review.id}"
      else
-       @my_review = my_review?(review)
-       redirect to '/review/new'
+      if !params[:review][:restroom_id].blank?
+        flash[:message] = "Hmm it seems you forgot to put a star rating."
+        redirect to "/review/new/#{params[:review][:restroom_id]}"
+      else
+        flash[:message] = "Please make sure to choose a location or add a new one and also a star rating"
+        @my_review = my_review?(review)
+        redirect to '/review/new'
+      end
      end
    end
 
