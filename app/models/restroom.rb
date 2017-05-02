44170e61ebfeb51fbe635031da0e722229cf989e
Omar Mason <mr.omarmason@gmail.com>
2017-05-02 21:47:38 +0000

    fix restrooms link. fix the write review page to reflect the controller page. rounded reviews to one decimal point.

--- app/models/restroom.rb
+++ app/models/restroom.rb
@@ -3,7 +3,7 @@ class Restroom < ActiveRecord::Base
   has_many :users, through: :reviews
 
   def rating
-    self.reviews.map(&:stars).map(&:to_f).reduce(:+)/self.reviews.all.count
+    (self.reviews.map(&:stars).map(&:to_f).reduce(:+)/self.reviews.all.count).round(1)           
   end
 
 end
